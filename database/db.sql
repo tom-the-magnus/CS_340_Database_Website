db9363ae0cabb3b072d3dfbb8a4a06b9ecc8b804
Thomas Magnus <magnust@flip3.engr.oregonstate.edu>
2023-11-09 13:14:06 -0800

    First upload of updated app

---
+++ database/db.sql
@@ -0,0 +1,239 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1-1.el7.remi
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Oct 27, 2023 at 02:16 PM
+-- Server version: 10.6.15-MariaDB-log
+-- PHP Version: 8.2.11
+
+SET FOREIGN_KEY_CHECKS=0;
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cs340_magnust`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Customers`
+--
+
+CREATE TABLE `Customers` (
+  `customer_id` int(11) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+--
+-- Dumping data for table `Customers`
+--
+
+INSERT INTO `Customers` (`customer_id`, `first_name`, `last_name`, `email`) VALUES
+(1, 'Chris', 'Cornell', 'spoonman@gmail.com'),
+(2, 'Kurt', 'Cobain', 'teenspirit@gmail.com'),
+(3, 'Scott', 'Weiland', 'bigempty@gmail.com');
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Genres`
+--
+
+CREATE TABLE `Genres` (
+  `genre_id` int(11) NOT NULL,
+  `genre_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+--
+-- Dumping data for table `Genres`
+--
+
+INSERT INTO `Genres` (`genre_id`, `genre_name`) VALUES
+(1, 'Action'),
+(2, 'Sci-Fi/Fantasy'),
+(3, 'Thriller');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Movies`
+--
+
+CREATE TABLE `Movies` (
+  `movie_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+--
+-- Dumping data for table `Movies`
+--
+
+INSERT INTO `Movies` (`movie_id`, `title`) VALUES
+(1, 'Gladiator'),
+(2, 'Jurassic Park'),
+(3, 'Silence of the Lambs'),
+(4, 'The Killing of a Sacred Deer'),
+(5, 'Aliens');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Movie_Genre_Details`
+--
+
+CREATE TABLE `Movie_Genre_Details` (
+  `movie_genre_id` int(11) NOT NULL,
+  `movie_id` int(11) DEFAULT NULL,
+  `genre_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+--
+-- Dumping data for table `Movie_Genre_Details`
+--
+
+INSERT INTO `Movie_Genre_Details` (`movie_genre_id`, `movie_id`, `genre_id`) VALUES
+(1, 1, 2),
+(2, 2, 2),
+(3, 3, 3),
+(4, 4, 3),
+(5, 5, 1),
+(6, 5, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Rentals`
+--
+
+CREATE TABLE `Rentals` (
+  `rental_id` int(11) NOT NULL,
+  `customer_id` int(11) DEFAULT NULL,
+  `movie_id` int(11),
+  `due_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+--
+-- Dumping data for table `Rentals`
+--
+
+INSERT INTO `Rentals` (`rental_id`, `customer_id`, `movie_id`, `due_date`) VALUES
+(1, 1, 2, '2023-11-07'),
+(2, 1, 1, '2023-11-27'),
+(3, 2, 1, '2023-12-08'),
+(4, 3, 3, '2023-12-24'),
+(5, 3, 5, '2024-01-14');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Customers`
+--
+ALTER TABLE `Customers`
+  ADD PRIMARY KEY (`customer_id`),
+  ADD UNIQUE KEY `customer_id` (`customer_id`);
+
+--
+-- Indexes for table `Genres`
+--
+ALTER TABLE `Genres`
+  ADD PRIMARY KEY (`genre_id`),
+  ADD UNIQUE KEY `genre_id` (`genre_id`);
+
+--
+-- Indexes for table `Movies`
+--
+ALTER TABLE `Movies`
+  ADD PRIMARY KEY (`movie_id`),
+  ADD UNIQUE KEY `movie_id` (`movie_id`);
+
+--
+-- Indexes for table `Movie_Genre_Details`
+--
+ALTER TABLE `Movie_Genre_Details`
+  ADD PRIMARY KEY (`movie_genre_id`),
+  ADD UNIQUE KEY `movie_genre_id` (`movie_genre_id`),
+  ADD KEY `movie_id` (`movie_id`),
+  ADD KEY `genre_id` (`genre_id`);
+
+--
+-- Indexes for table `Rentals`
+--
+ALTER TABLE `Rentals`
+  ADD PRIMARY KEY (`rental_id`),
+  ADD UNIQUE KEY `rental_id` (`rental_id`),
+  ADD KEY `customer_id` (`customer_id`),
+  ADD KEY `movie_id` (`movie_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Customers`
+--
+ALTER TABLE `Customers`
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `Genres`
+--
+ALTER TABLE `Genres`
+  MODIFY `genre_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `Movies`
+--
+ALTER TABLE `Movies`
+  MODIFY `movie_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `Movie_Genre_Details`
+--
+ALTER TABLE `Movie_Genre_Details`
+  MODIFY `movie_genre_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `Rentals`
+--
+ALTER TABLE `Rentals`
+  MODIFY `rental_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Movie_Genre_Details`
+--
+ALTER TABLE `Movie_Genre_Details`
+  ADD CONSTRAINT `Movie_Genre_Details_ibfk_1` FOREIGN KEY (`movie_id`) REFERENCES `Movies` (`movie_id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `Movie_Genre_Details_ibfk_2` FOREIGN KEY (`genre_id`) REFERENCES `Genres` (`genre_id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `Rentals`
+--
+ALTER TABLE `Rentals`
+  ADD CONSTRAINT `Rentals_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `Customers` (`customer_id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `Rentals_ibfk_2` FOREIGN KEY (`movie_id`) REFERENCES `Movies` (`movie_id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+SET FOREIGN_KEY_CHECKS=1
---
+++ webapp.py
@@ -0,0 +1,394 @@
+# Sample Flask application for a BSG database, snapshot of BSG_people
+
+from flask import Flask, render_template, json, redirect
+from flask_mysqldb import MySQL
+from flask import request
+import os
+
+
+app = Flask(__name__)
+
+# database connection
+# Template:
+# app.config["MYSQL_HOST"] = "classmysql.engr.oregonstate.edu"
+# app.config["MYSQL_USER"] = "cs340_OSUusername"
+# app.config["MYSQL_PASSWORD"] = "XXXX" | last 4 digits of OSU id
+# app.config["MYSQL_DB"] = "cs340_OSUusername"
+# app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+
+# database connection info
+app.config["MYSQL_HOST"] = "classmysql.engr.oregonstate.edu"
+app.config["MYSQL_USER"] = "cs340_magnust"
+app.config["MYSQL_PASSWORD"] = "2723"
+app.config["MYSQL_DB"] = "cs340_magnust"
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+
+mysql = MySQL(app)
+
+# Routes
+# have homepage route
+@app.route("/index")
+def index():
+    return render_template("index.j2")
+
+@app.route("/")
+def home():
+    return redirect("/index")
+
+# route for Customers page
+@app.route("/customers", methods=["POST", "GET"])
+def customers():
+    # Separate out the request methods, in this case this is for a POST
+    # insert a customer into the customer entity
+    if request.method == "POST":
+        # fire off if user presses the Add Customer button
+        if request.form.get("Add_Customer"):
+            # grab user form inputs
+            first_name = request.form["first_name"]
+            last_name = request.form["last_name"]
+            email = request.form["email"]
+
+
+            query = "INSERT INTO Customers (first_name, last_name, email) VALUES (%s, %s,%s)"
+            cur = mysql.connection.cursor()
+            cur.execute(query, (first_name, last_name, email))
+            mysql.connection.commit()
+
+            # redirect back to people page
+            return redirect("/customers")
+
+    # Grab Customers data so we send it to our template to display
+    if request.method == "GET":
+        # mySQL query to grab all the customers in Customers
+        query = "SELECT customer_id, first_name, last_name, email FROM Customers"
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+
+        if not data:
+            return render_template("insert_customer.j2", data=data)
+
+        # render edit_customers page passing our query data and rental data to the edit_customers template
+        return render_template("customers.j2", data=data)
+
+
+# route for delete functionality, deleting a person from bsg_people,
+# we want to pass the 'id' value of that person on button click (see HTML) via the route
+@app.route("/delete_customers/<int:customer_id>")
+def delete_customers(customer_id):
+    # mySQL query to delete the person with our passed id
+    query = "DELETE FROM Customers WHERE customer_id = '%s';"
+    cur = mysql.connection.cursor()
+    cur.execute(query, (customer_id,))
+    mysql.connection.commit()
+
+    # redirect back to people page
+
+    return redirect("/customers")
+
+# route for edit functionality, updating the attributes of a person in bsg_people
+# similar to our delete route, we want to the pass the 'id' value of that person on button click (see HTML) via the route
+@app.route("/edit_customers/<int:customer_id>", methods=["POST", "GET"])
+def edit_customers(customer_id):
+    if request.method == "GET":
+        # mySQL query to grab the info of the person with our passed id
+        query = "SELECT * FROM Customers WHERE customer_id = %s" % (customer_id)
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+
+        # render edit_people page passing our query data and homeworld data to the edit_people template
+        return render_template("edit_customers.j2", data=data)
+
+    # meat and potatoes of our update functionality
+    if request.method == "POST":
+        # fire off if user clicks the 'Edit Person' button
+        if request.form.get("Edit_Customer"):
+            # grab user form inputs
+            customer_id = request.form["customer_id"]
+            first_name = request.form["first_name"]
+            last_name = request.form["last_name"]
+            email = request.form["email"]
+
+            query = "UPDATE Customers SET Customers.first_name = %s, Customers.last_name = %s, Customers.email = %s WHERE Customers.customer_id = %s"
+            cur = mysql.connection.cursor()
+            cur.execute(query, (first_name, last_name, email, customer_id))
+            mysql.connection.commit()
+
+            # redirect back to people page after we execute the update query
+            return redirect("/customers")
+
+# route for Movies page
+@app.route("/movies", methods=["POST", "GET"])
+def movies():
+    # Separate out the request methods, in this case this is for a POST
+    # insert a movie into the Movie entity
+    if request.method == "POST":
+        # fire off if user presses the Add Movie button
+        if request.form.get("Add_Movie"):
+            # grab user form inputs
+            title = request.form["title"]
+
+            query = "INSERT INTO Movies (title) VALUES (%s)"
+            cur = mysql.connection.cursor()
+            cur.execute(query, (title,))
+            mysql.connection.commit()
+
+            # redirect back to Movies page
+            return redirect("/movies")
+
+    # Grab Movies data so we send it to our template to display
+    if request.method == "GET":
+        # mySQL query to grab all the movies in Movies
+        query = "SELECT movie_id, title FROM Movies"
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+
+
+        # render edit_movies page passing our query data to the edit_movies template
+        return render_template("movies.j2", data=data)
+
+
+# route for delete functionality, deleting a movie from Movies,
+# we want to pass the 'id' value of that movie on button click (see HTML) via the route
+@app.route("/delete_movies/<int:movie_id>")
+def delete_movies(movie_id):
+    # mySQL query to delete the movie with our passed movie_id
+    query = "DELETE FROM Movies WHERE movie_id = '%s';"
+    cur = mysql.connection.cursor()
+    cur.execute(query, (movie_id,))
+    mysql.connection.commit()
+
+    # redirect back to people page
+    return redirect("/movies")
+
+# route for Genres page
+@app.route("/genres", methods=["POST", "GET"])
+def genres():
+    # Separate out the request methods, in this case this is for a POST
+    # insert a genre into the Genre entity
+    if request.method == "POST":
+        # fire off if user presses the Add Genre button
+        if request.form.get("Add_Genre"):
+            # grab user form inputs
+            genre_name = request.form["genre_name"]
+
+            query = "INSERT INTO Genres (genre_name) VALUES (%s)"
+            cur = mysql.connection.cursor()
+            cur.execute(query, (genre_name,))
+            mysql.connection.commit()
+
+            # redirect back to Genres page
+            return redirect("/genres")
+
+    # Grab Genres data so we send it to our template to display
+    if request.method == "GET":
+        # mySQL query to grab all the genres in Genres
+        query = "SELECT genre_id, genre_name FROM Genres"
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+
+        # render edit_genres page passing our query data to the edit_genres template
+        return render_template("genres.j2", data=data)
+
+# route for delete functionality, deleting a genre from Genres,
+# we want to pass the 'id' value of that genre on button click (see HTML) via the route
+@app.route("/delete_genres/<int:genre_id>")
+def delete_genres(genre_id):
+    # mySQL query to delete the person with our passed id
+    query = "DELETE FROM Genres WHERE genre_id = '%s';"
+    cur = mysql.connection.cursor()
+    cur.execute(query, (genre_id,))
+    mysql.connection.commit()
+
+    # redirect back to genres page
+    return redirect("/genres")
+
+@app.route("/movie_genre_details", methods=["POST", "GET"])
+def movie_genre_details():
+    cur = mysql.connection.cursor()
+
+    if request.method == "POST":
+        # fire off if user presses the Add movie_genre_details button
+        if request.form.get("Add_Movie_Genre_Detail"):
+            # grab user form inputs
+            genre_id = request.form["genre_id"]
+            movie_id = request.form["movie_id"]
+
+            query = "INSERT INTO Movie_Genre_Details (genre_id, movie_id) VALUES (%s,%s)"
+            cur.execute(query, (genre_id, movie_id))
+            mysql.connection.commit()
+
+            # redirect back to movie_genre_details page
+            return redirect("/movie_genre_details")
+
+    # Handle GET request
+    # Fetch all movie_genre_details
+    details_query = "SELECT movie_genre_id, movie_id, genre_id FROM Movie_Genre_Details"
+    cur.execute(details_query)
+    details = cur.fetchall()
+
+    # Fetch all movies
+    movies_query = "SELECT movie_id, title FROM Movies"
+    cur.execute(movies_query)
+    movies = cur.fetchall()
+
+    # Fetch all genres
+    genres_query = "SELECT genre_id, genre_name FROM Genres"
+    cur.execute(genres_query)
+    genres = cur.fetchall()
+
+    # Pass fetched data to the template
+    return render_template("movie_genre_details.j2", details=details, movies=movies, genres=genres)
+
+'''
+# !!!!! OLD WORKING ROUTE !!!!!!!!!!!! 
+# route for Movie_Genre_Details page
+# route for Movie_Genre_Details page
+@app.route("/movie_genre_details", methods=["POST", "GET"])
+def movie_genre_details():
+    # Separate out the request methods, in this case this is for a POST
+    # insert a customer into the customer entity
+    if request.method == "POST":
+        # fire off if user presses the Add movie_genre_details button
+        if request.form.get("Add_Movie_Genre_Detail"):
+            # grab user form inputs
+            genre_id = request.form["genre_id"]
+            movie_id = request.form["movie_id"]
+
+            query = "INSERT INTO Movie_Genre_Details (genre_id, movie_id) VALUES (%s,%s)"
+            cur = mysql.connection.cursor()
+            cur.execute(query, (genre_id, movie_id))
+            mysql.connection.commit()
+
+            # redirect back to movie_genre_details page
+            return redirect("/mov~ie_genre_details")
+
+    # Grab Movie_Genre_Details data so we send it to our template to display
+    if request.method == "GET":
+        # mySQL query to grab all the customers in Customers
+        query = "SELECT movie_genre_id, movie_id, genre_id FROM Movie_Genre_Details"
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+
+
+        # render edit_customers page passing our query data and rental data to the edit_customers template
+        return render_template("movie_genre_details.j2", data=data)
+    '''
+
+    # # Grab Movie_Genre_Details data so we send it to our template to display
+    # if request.method == "GET":
+    #     # mySQL query to grab all the customers in Customers
+    #     movies_query = "SELECT movie_id, title FROM Movies" 
+    #     cur.execute(movies_query)
+    #     movies = cur.fetchall()
+
+    #     genres_query = "SELECT genre_id, genre_name FROM Genres"
+    #     cur.execute(genres_query)
+    #     genres = cur.fetchall()
+
+    #     # Fetch existing movie_genre_details    
+    #     details_query = "SELECT movie_genre_id, movie_id, genre_id FROM Movie_Genre_Details"
+    #     cur.execute(details_query)
+    #     details = cur.fetchall()
+
+    #     # Pass movies, genres, and details to the template
+    #     return render_template("movie_genre_details.j2", movies=movies, genres=genres, details=details)
+
+
+@app.route("/edit_movie_genre_details/<int:movie_genre_id>", methods=["POST", "GET"])
+def edit_movie_genre_details(movie_genre_id):
+    if request.method == "GET":
+        cur = mysql.connection.cursor()
+
+        # get the genre-id 
+        movie_genre_query = "SELECT * FROM Movie_Genre_Details WHERE movie_genre_id = %s"
+        cur.execute(movie_genre_query, (movie_genre_id,))
+        movie_genre_detail = cur.fetchone()
+
+        # get all movies 
+        movies_query = "SELECT movie_id, title FROM Movies"
+        cur.execute(movies_query)
+        movies = cur.fetchall()
+
+        # get all genres
+        genres_query = "SELECT genre_id, genre_name FROM Genres"
+        cur.execute(genres_query)
+        genres = cur.fetchall()
+
+        # pass data to template
+        return render_template("edit_movie_genre_details.j2",
+                               movie_genre_detail=movie_genre_detail,
+                               movies=movies,   
+                               genres=genres)
+    
+    return redirect("/movie_genre_details")
+
+# route for delete functionality, deleting a person from bsg_people,
+# we want to pass the 'id' value of that person on button click (see HTML) via the route
+@app.route("/delete_movie_genre_details/<int:movie_genre_id>")
+def delete_movie_genre_details(movie_genre_id):
+    # mySQL query to delete the person with our passed id
+    query = "DELETE FROM Movie_Genre_Details WHERE movie_genre_id = '%s';"
+    cur = mysql.connection.cursor()
+    cur.execute(query, (movie_genre_id,))
+    mysql.connection.commit()
+
+    # redirect back to people page
+    return redirect("/movie_genre_details")
+
+# route for Rentals page
+@app.route("/rentals", methods=["POST", "GET"])
+def rentals():
+    # Separate out the request methods, in this case this is for a POST
+    # insert a rental into the Rentals entity
+    if request.method == "POST":
+        # fire off if user presses the Add Rental button
+        if request.form.get("Add_Rental"):
+            # grab user form inputs
+            customer_id = request.form["customer_id"]
+            movie_id = request.form["movie_id"]
+            due_date = request.form["due_date"]
+
+
+            query = "INSERT INTO Rentals (customer_id, movie_id, due_date) VALUES (%s, %s,%s)"
+            cur = mysql.connection.cursor()
+            cur.execute(query, (customer_id, movie_id, due_date))
+            mysql.connection.commit()
+
+            # redirect back to people page
+            return redirect("/rentals")
+
+    # Grab Customers data so we send it to our template to display
+    if request.method == "GET":
+        # mySQL query to grab all the customers in Customers
+        query = "SELECT rental_id, customer_id, movie_id, due_date FROM Rentals"
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+
+
+        # render edit_customers page passing our query data and rental data to the edit_customers template
+        return render_template("rentals.j2", data=data)
+
+
+# route for delete functionality, deleting a person from bsg_people,
+# we want to pass the 'id' value of that person on button click (see HTML) via the route
+@app.route("/delete_rentals/<int:rental_id>")
+def delete_rentals(rental_id):
+    # mySQL query to delete the person with our passed id
+    query = "DELETE FROM Rentals WHERE rental_id = '%s';"
+    cur = mysql.connection.cursor()
+    cur.execute(query, (rental_id,))
+    mysql.connection.commit()
+
+    # redirect back to people page
+    return redirect("/rentals")
+
+# Listener
+# change the port number if deploying on the flip servers
+if __name__ == "__main__":
+    app.run(port=3243, debug=True)
